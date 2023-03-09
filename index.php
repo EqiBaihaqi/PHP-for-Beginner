480145520ee77dc74fa7fc78e16ec50bbc140685
EqiBaihaqi <baihaqi08.aa@gmail.com>
2023-03-09 08:18:53 +0700

    Episode 6 - Array

--- index.php
+++ index.php
@@ -3,29 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <style>
-        body{
-        display: grid;
-        place-items: center;
-        height: 100vh;
-        font-family: sans-serif;
-        margin: 0;
-        }
-    </style>
 </head>
 <body>
-    <?php 
-    $name = "Dark Matter";
-    $read = false;
-
-    if($read){
-        $message = "You have read $name";
-    }else{
-        $message = "You have NOT read $name";
-    }
-    ?>
     <h1>
-       <?= $message ?>
+        Recommendation books
     </h1>
+
+    <?php 
+    $books = [
+        "Do Androids Dream of Electric Shop",
+        "The Langoliers",
+        "Hail Mary"
+    ];
+    ?>
+
+    <ul>
+        <?php foreach($books as $book) {
+            echo "<li>{$book}™</li>";
+        };
+        
+        ?>
+    </ul>
+    
 </body>
 </html>
